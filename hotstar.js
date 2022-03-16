e7fce0300dbc156777035a2f368828f215c70944
Pradeep22 <pradeepgoud.rpg@gmail.com>
2022-03-16 17:30:52 +0530

    coupon

---
+++ hotstar.js
@@ -0,0 +1,6 @@
+function loadcoupon(){
+    document.getElementById('coupon').style.visibility='visible'
+}
+function closecoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden'
+}
